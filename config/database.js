e144a1f4b041ffba903fcfbe93cab50f171af82c
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2021-09-17 02:19:57 +0500

    added env

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         uri: env("DATABASE_URI"),
+        // uri: "mongodb+srv://Subhan:b11ysOaEOk1SJEWi@natours.t69zc.mongodb.net/CreativeBooks?retryWrites=true&w=majority",
       },
       options: {
         ssl: true,
