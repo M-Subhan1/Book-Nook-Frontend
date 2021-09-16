7565837305e44d35e7b64d8356a8f9376ebd8f1d
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2021-09-17 02:40:12 +0500

    updated url

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 8000),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '27495170e52b034e07fe621e66cda56e'),
+      secret: env("ADMIN_JWT_SECRET", "27495170e52b034e07fe621e66cda56e"),
     },
   },
 });
