9254a82a77717fe4c119e4f6d04768768b4512f9
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2021-09-19 05:10:04 +0500

    added cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -11,5 +11,13 @@ module.exports = ({ env }) => ({
       defaultReplyTo: "no-reply@creativebooks.pk",
       testAddress: "m.subhanabbas@gmail.com",
     },
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_API_KEY"),
+        api_secret: env("CLOUDINARY_API_SECRET"),
+      },
+    },
   },
 });
